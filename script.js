596346be106a2fbaba6c6cf660b955b5d4aa8158
Oskar Krajniczyn <55380790+oskarkraj@users.noreply.github.com>
2020-05-12 12:11:39 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-const release = document.querySelector ('.release');
-const containerCatalogues = document.querySelector ('.container-catalogues');
-
-const handleClick = () => {
-    release.classList.toggle('release-active');
-    containerCatalogues.classList.toggle('container-catalogues-active');
-}
-release.addEventListener('click', handleClick);
